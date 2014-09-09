665bd3691cecc246ef51c05c9bf6f505b47a7f43
chenfeng <chenfeng@chukong-inc.com>
2014-09-09 15:59:56 +0800

    Initial and Add Sample for C++

---
+++ Sample_C++/pro.android/obj/local/armeabi-v7a/objs/game_shared/Ads.o.d
@@ -0,0 +1,72 @@
+./obj/local/armeabi-v7a/objs/game_shared/Ads.o: jni/Ads.cpp jni/Ads.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/AgentManager.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginManager.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginProtocol.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginParam.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginFactory.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolIAP.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAnalytics.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolShare.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAds.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolSocial.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolUser.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolPush.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/list \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/jni.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/android/log.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/PluginJniHelper.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/stdlib.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/alloca.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/strings.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/memory.h
+
+jni/Ads.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/AgentManager.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginManager.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginProtocol.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginParam.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginFactory.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolIAP.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAnalytics.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolShare.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAds.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolSocial.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolUser.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolPush.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/list:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/jni.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/android/log.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/PluginJniHelper.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/stdlib.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/alloca.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/strings.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/memory.h:
---
+++ Sample_C++/pro.android/obj/local/armeabi/objs/game_shared/main.o.d
@@ -0,0 +1,79 @@
+./obj/local/armeabi/objs/game_shared/main.o: jni/main.cpp \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/jni.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/sys/cdefs.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/android/api-level.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/android/log.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/PluginJniHelper.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+  jni/PluginChannel.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/AgentManager.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginManager.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginProtocol.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginParam.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginFactory.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolIAP.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAnalytics.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolShare.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAds.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolSocial.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolUser.h \
+  /Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolPush.h \
+  /Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/list \
+  jni/Analytics.h jni/Ads.h jni/Share.h jni/Social.h
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/jni.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/android/api-level.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/platforms/android-17/arch-arm/usr/include/android/log.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/PluginJniHelper.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+jni/PluginChannel.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/AgentManager.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginManager.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginProtocol.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginParam.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/PluginFactory.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolIAP.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAnalytics.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolShare.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolAds.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolSocial.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolUser.h:
+
+/Users/daisy/git/anysdk_framework/samples/Sample/pro.android/protocols/android/../include/ProtocolPush.h:
+
+/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.8/include/list:
+
+jni/Analytics.h:
+
+jni/Ads.h:
+
+jni/Share.h:
+
+jni/Social.h:
